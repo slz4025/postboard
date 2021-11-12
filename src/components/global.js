4cf5245fe2d00ac55677d51aaf0220aa258741e2
Sydney Zheng <tlz9472@gmail.com>
2021-11-12 08:49:11 -0800

    use published backend

--- src/components/global.js
+++ src/components/global.js
@@ -1,1 +1,2 @@
-export const serverPoint = "http://127.0.0.1:8787";
+export const serverPoint = "https://postboard.slzheng.workers.dev/posts";
+export const devServerPoint = "http://127.0.0.1:8787";
